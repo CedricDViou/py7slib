4a69089b3f7e660668796cf6018728637dc0d80c
Dave DeBoer <ddeboer@berkeley.edu>
2021-09-28 15:01:09 -0700

    add setup note about stuff moved out to hera_node_mc

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@ from setuptools import setup, find_packages
 packages = find_packages()
 
 # Also want to replicate what install.sh is doing as a separate install script.
+#  The module wr_len.py and the script hera_wr_redis_monitor.py moved to node_control.
 
 setup_args = dict(name="py7slib",
                   maintainer="HERA Team",
