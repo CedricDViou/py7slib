558f856e1ebc021c596df98b2364097040b98823
Cedric Viou <cedric.viou@obs-nancay.fr>
2024-12-13 15:53:32 +0000

    rename module name

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name='py7slib-py3',
+    name='py7slib_py3',
     version='0.0.1',
     description='Python3 version of py7slib',
     author='Cedric Viou',
@@ -13,11 +13,11 @@ setup(
     ],
     entry_points={
         'console_scripts': [
-            'eb_sw_loader=py7slib-py3.eb_sw_loader:main',
-            'sdb_tool=py7slib-py3.sdb_tool:main',
-            'shell=py7slib-py3.shell:main',
-            'spiflash_update=py7slib-py3.spiflash_update:main',
-            'write_calibration=py7slib-py3.write_calibration:main',
+            'eb_sw_loader=py7slib_py3.eb_sw_loader:main',
+            'sdb_tool=py7slib_py3.sdb_tool:main',
+            'shell=py7slib_py3.shell:main',
+            'spiflash_update=py7slib_py3.spiflash_update:main',
+            'write_calibration=py7slib_py3.write_calibration:main',
         ],
     },
 )
